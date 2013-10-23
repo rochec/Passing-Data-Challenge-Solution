79e8472236ad5070ea20cc2b24287c9465d9da4d
rochec <rochec@gmail.com>
2013-10-23 17:00:01 -0500

    Initial Commit

---
+++ Passing Data Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Passing Data Challenge Solution
+//
+//  Created by Chris Roche on 10/23/13.
+//  Copyright (c) 2013 Chris Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
